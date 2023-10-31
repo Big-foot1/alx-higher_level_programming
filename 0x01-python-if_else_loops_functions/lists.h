6b9deff1b129c8a98ee8fc421e0e28e2b7f04f90
Big-foot1 <imutyaba11@gmail.com>
2023-10-31 14:52:01 -0700

    12-fizzbzz

--- 0x01-python-if_else_loops_functions/lists.h
+++ 0x01-python-if_else_loops_functions/lists.h
@@ -20,6 +20,7 @@ def uppercase(str):
 def print_last_digit(number):
 def add(a, b):
 def pow(a, b):
+def fizzbuzz():
 size_t print_listint(const listint_t *h);
 listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
