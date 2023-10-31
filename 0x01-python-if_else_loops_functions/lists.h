984168ed3bd6a9bd186b6a602a3b8094dc1f449e
Big-foot1 <imutyaba11@gmail.com>
2023-10-31 14:41:12 -0700

    computing power

--- 0x01-python-if_else_loops_functions/lists.h
+++ 0x01-python-if_else_loops_functions/lists.h
@@ -19,6 +19,7 @@ def islower(c):
 def uppercase(str):
 def print_last_digit(number):
 def add(a, b):
+def pow(a, b):
 size_t print_listint(const listint_t *h);
 listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
