b2518a9a6d5f33e2d126c440ee7e85312ce7e560
Big-foot1 <imutyaba11@gmail.com>
2023-12-11 21:34:09 +0000

    1. Base class

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Base Module
+"""
+import json
+import csv
+
+
+class Base:
+    """
+    The Base Class
+    Attributes:
+        __nb_object : private class atribute
+    """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """
+        Init
+        Attributes:
+            id (): id
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
