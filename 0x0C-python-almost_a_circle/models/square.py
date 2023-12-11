402fb8beb0812ac679c9cd006b813f8013436761
Big-foot1 <imutyaba11@gmail.com>
2023-12-11 22:46:15 +0000

    12. Square update

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -29,3 +29,21 @@ class Square(Rectangle):
         """
         self.height = value
         self.width = value
+
+    def update(self, *args, **kwargs):
+        """update"""
+        i = 0
+        if args:
+            for arg in args:
+                if i == 0:
+                    self.id = arg
+                if i == 1:
+                    self.size = arg
+                if i == 2:
+                    self.x = arg
+                if i == 3:
+                    self.y = arg
+                i += 1
+        else:
+            for arg in kwargs:
+                setattr(self, arg, kwargs.get(arg))
