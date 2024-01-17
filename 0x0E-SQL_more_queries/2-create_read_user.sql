db9b7e11bc161b890d088057a1a2a35fa9a69b42
Big-foot1 <imutyaba11@gmail.com>
2024-01-17 12:02:08 -0800

    create db

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -4,4 +4,4 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost'
 IDENTIFIED BY 'user_0d_2_pwd';
 -- provide only select previlege to the user
-GRANT select ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
