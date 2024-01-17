330570c3b3f5e11a971568c076883b6dbffab788
Big-foot1 <imutyaba11@gmail.com>
2024-01-17 11:55:23 -0800

    create db

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,7 @@
+-- create a database
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+-- create user to access the database
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost'
+IDENTIFIED BY 'user_0d_2_pwd';
+-- provide only select previlege to the user
+GRANT select ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
