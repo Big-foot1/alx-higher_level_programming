afe85907fecc001fef7f91d1756002a974318ae5
Big-foot1 <imutyaba11@gmail.com>
2024-03-04 07:44:21 -0800

    task0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,11 +5,11 @@ fetch intranet status page
 import urllib.request
 
 if __name__ == '__main__':
-    req = urllib.request.Request("https://intranet.hbtn.io/status")
+    req = "https://intranet.hbtn.io/status"
     with urllib.request.urlopen(req) as response:
-        html = response.read()
+        body = response.read()
 
     print("Body response:")
-    print("\t- type: {}".format(type(html)))
-    print("\t- content: {}".format(html))
-    print("\t- utf8 content: {}".format(html.decode("utf-8")))
+    print("\t- type: {}".format(body.__class__))
+    print("\t- content: {}".format(body))
+    print("\t- utf8 content: {}".format(body.decode("utf-8")))
